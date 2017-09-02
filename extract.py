73178f5bb5d072c9393bf8ec35a06c8395238a72
Michal Vyskocil <michal.vyskocil@gmail.com>
2017-09-02 17:09:36 +0200

    Problem: too many end of lines in otazky.json
    Solution: drop them

--- extract.py
+++ extract.py
@@ -52,7 +52,7 @@ class MyHTMLParser(HTMLParser):
             self._handle_starttag = self._find_question_b
 
     def _print_answer (self, data):
-        answer = data.strip ()
+        answer = data.strip ().replace ('\n', '')
         if answer [-1] == ',' or answer [-1] == '.':
             answer = answer [:-1]
         self._questions [-1] ["answers"].append (
